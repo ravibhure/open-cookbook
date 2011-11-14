4ea1b75bf03654e7487ef235f73c29d81c33b6a3
Ravi Bhure <ravibhure@gmail.com>
2011-11-14 17:20:54 +0530

    updates dir set in setup_mon

--- db_postgres/recipes/setup_monitoring.rb
+++ db_postgres/recipes/setup_monitoring.rb
@@ -21,9 +21,9 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-db_postgres node[:db_postgres][:data_dir] do
- directory node[:db_postgres][:collectd_plugin_dir] do
+directory node[:db_postgres][:collectd_plugin_dir] do
   action :create
 end
+db_postgres node[:db_postgres][:data_dir] do
   action :setup_monitoring
 end
