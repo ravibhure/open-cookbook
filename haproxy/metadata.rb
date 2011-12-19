3d02721b857e9f0c7ba5d0eb2f8b830ec48b2d5a
Ravi Bhure <ravibhure@gmail.com>
2011-12-19 08:40:10 -0500

    test template condition

--- haproxy/metadata.rb
+++ haproxy/metadata.rb
@@ -5,7 +5,15 @@ description       "Installs and configures haproxy"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version           "0.0.1"
 recipe "haproxy::default", "HAproxy recipe, Installs and configures HAproxy"
+recipe "haproxy::dbsetup", "HAproxy recipe, configures HAproxy db"
 
 %w{ redhat centos fedora }.each do |os|
   supports os
 end
+
+attribute "haproxy/db_adapter",
+  :display_name => "Database Adapter",
+  :description => "Database adapter to connect to Database. (Ex: mysql)",
+  :default => "postgres",
+  :choice => [ "mysql", "postgres" ],
+  :recipes => [ "haproxy::dbsetup" ]
